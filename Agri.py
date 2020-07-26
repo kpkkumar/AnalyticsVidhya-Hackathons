d1a86a0f4359fac5d84d5032aa23b4f057efe9ca
Kanchana Kumar Kaliappan <45136819+kpkkumar@users.noreply.github.com>
2020-07-26 13:47:14 +0530

    Update Agri.py
    Final Version

--- Agri.py
+++ Agri.py
@@ -21,9 +21,9 @@ train['Pesticide_Use_Category'] = train['Pesticide_Use_Category'].astype('catego
 
 train.isnull().sum()
 
-train.Number_Weeks_Used.mean()
+train['Number_Weeks_Used'].describe()
 
-train['Number_Weeks_Used']=train['Number_Weeks_Used'].fillna(value=28)
+train['Number_Weeks_Used']=train['Number_Weeks_Used'].fillna(value=32)
 
 train['Number_Weeks_Used']=train['Number_Weeks_Used'].astype('int64')
 
@@ -45,34 +45,124 @@ sns.boxplot(train.Number_Weeks_Used)
 
 train=train[train.Number_Weeks_Used<60]
 
+
 sns.boxplot(train.Number_Weeks_Quit)
 
 train=train[train.Number_Weeks_Quit<40]
 
 train.describe()
 
+
+
 sns.countplot(x=train.Crop_Damage)
+
 
 sns.countplot(x=train.Season)
 
+
+
+
 sns.countplot(x=train.Pesticide_Use_Category)
+
+
+
+
 
 sns.countplot(x=train.Crop_Type)
 
+
+
+
 sns.countplot(x=train.Soil_Type)
+
+
+
+
 
 table= pd.crosstab(train.Soil_Type, train.Crop_Damage)
 
 table.div(table.sum(1).astype(float), axis = 0).plot(kind='bar', stacked = 'True')
 
+
+
+
 table= pd.crosstab(train.Crop_Type, train.Crop_Damage)
 
 table.div(table.sum(1).astype(float), axis = 0).plot(kind='bar', stacked = 'True')
+
+
+
 
 table= pd.crosstab(train.Season, train.Crop_Damage)
 
 table.div(table.sum(1).astype(float), axis = 0).plot(kind='bar', stacked = 'True')
 
+
 table= pd.crosstab(train.Pesticide_Use_Category, train.Crop_Damage)
 
 table.div(table.sum(1).astype(float), axis = 0).plot(kind='bar', stacked = 'True')
+
+train.Crop_Damage.value_counts()
+
+train.columns
+
+
+'''from sklearn.utils import resample
+major = train[train.Crop_Damage==1]
+minor = train[train.Crop_Damage==0]
+minor_upsample1=resample(minor, replace=True, n_samples=11214 )
+
+
+from sklearn.utils import resample
+major = train[train.Crop_Damage==0]
+minor = train[train.Crop_Damage==2]
+minor_upsample2=resample(minor, replace=True, n_samples=71430 )
+
+
+sample_train= pd.concat([major,minor_upsample1])
+
+sample_train.Crop_Damage.value_counts()
+
+sample_train.column'''
+
+xtrain=train[['Crop_Type','Estimated_Insects_Count','Pesticide_Use_Category','Number_Doses_Week', 'Number_Weeks_Used','Number_Weeks_Quit']]
+
+ytrain=train.Crop_Damage
+
+
+test=pd.read_csv("D:\Studies\Books\Data Science\Python Projects\Analytics Vidhya\Agriculture\Dataset\Test.csv")
+
+test.dtypes
+
+test['Crop_Type']= test['Crop_Type'].astype('category')
+
+test['Soil_Type']=test['Soil_Type'].astype('category')
+
+test['Season'] = test['Season'].astype('category')
+
+test['Pesticide_Use_Category'] = test['Pesticide_Use_Category'].astype('category')
+
+test.isnull().sum()
+
+test.Number_Weeks_Used.mean()
+
+test.Number_Weeks_Used=test.Number_Weeks_Used.fillna(value=32)
+
+testdata=test[['Crop_Type','Estimated_Insects_Count','Pesticide_Use_Category','Number_Doses_Week', 'Number_Weeks_Used','Number_Weeks_Quit']]
+
+from sklearn.linear_model import LogisticRegression
+
+from sklearn import metrics
+
+logreg = LogisticRegression()
+
+logreg.fit(xtrain, ytrain)
+
+prediction = logreg.predict(testdata)
+
+prediction = pd.DataFrame(prediction)
+
+prediction.to_csv(r'D:\Studies\Books\Data Science\Python Projects\Analytics Vidhya\Agriculture\Dataset\Output.csv')
+
+
+
